a39db88eb188eaaa6cbc12dcec26ad19b0197ca0
bshabani-UNCC <bshabani@uncc.edu>
2021-10-20 14:31:17 -0400

    Activity #9 Submission - Slideshow

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.forward').on('click', function(){
+        var currentImage = $('.first');
+        var nextImage = currentImage.next();
+
+        if(nextImage.length){
+            currentImage.removeClass('first').css('z-index', -10);
+            nextImage.addClass('first').css('z-index', 10);
+        }
+    });
+
+    $('.back').on('click', function(){
+        var currentImage = $('.first');
+        var previousImage = currentImage.prev();
+
+        if(previousImage.length){
+            currentImage.removeClass('first').css('z-index', -10);
+            previousImage.addClass('first').css('z-index', 10);
+        }
+    });
+});
