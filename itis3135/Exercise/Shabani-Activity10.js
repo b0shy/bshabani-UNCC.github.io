99698e31f46a985477d8c53ef251107bbcc46ac2
bshabani-UNCC <bshabani@uncc.edu>
2021-11-04 13:15:56 -0400

    Activity 10

--- itis3135/Exercise/Shabani-Activity10.js
+++ itis3135/Exercise/Shabani-Activity10.js
@@ -1,28 +1,30 @@
 $(document).ready(function () {
 
-        // preload the image for each link
-        $("image_list a").each(function () {
-                var imageSwapped = new Image();
-                imageSwapped.src = $(this).attr("href");
-        });
-
         // set up the event handlers for each link
-        $("image_list a").click(function (evt) {
+        $("#image_list a").click(function (evt){
+
+
+
                 // get the image URL and caption for each image and animate the caption
                 var imageLink = $(this).attr("href");
-                $("image").attr("src", imageLink);
                 var imageCaption = $(this).attr("title");
-                $("#caption").fadeOut(1000, function () {
-                        $("#caption").text(caption).fadeIn(1000);
+
+                $("#caption").fadeOut(1000, function (){
+                        $("#caption").text(imageCaption).fadeIn(1000);
                 });
-                $("#image").fadeOut(1000, function () {
-                        $("image").attr("src", imageLink).fadeIn(1000);
+
+                $("#image").fadeOut(1000, function (){
+                        $("#image").attr("src", imageLink).fadeIn(1000);
                 });
+
                 // cancel the default action of each link
                 evt.preventDefault();
+
         });
 
         // move the focus to the first link
         $("li:first-child a").focus();
+        $("#image").hide().fadeIn(1000);
+        $("#caption").hide().fadeIn(1000);
 
 }); // end ready
