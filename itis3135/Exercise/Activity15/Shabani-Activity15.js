bd950a4c425712aa23492ed5881c3b2c202d5ec6
bshabani-UNCC <bshabani@uncc.edu>
2021-11-22 13:13:36 -0500

    Activity 15

--- itis3135/Exercise/Activity15/Shabani-Activity15.js
+++ itis3135/Exercise/Activity15/Shabani-Activity15.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
             $.each(this, function(key, value){
                 $("#faculty").append(
                     "<img src="+"'"+ value.image +"'><br>"+
-                    "<h3>"+ value.full_name +"</h3>"+
+                    "<h2>"+ value.full_name +"</h2>"+
                     "<h3>"+ value.department +"</h3>"+
                     "<p>" + value.bio +"</p><br>"
                 );
