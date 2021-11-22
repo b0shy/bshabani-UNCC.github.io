9d0eeeb2913b4017cb6191c31486f83fd08a2429
bshabani-UNCC <bshabani@uncc.edu>
2021-11-22 13:07:49 -0500

    Activity 15

---
+++ itis3135/Exercise/Activity15/Shabani-Activity15.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $.getJSON("facultyList.json", function(data){
+        $.each(data, function(){
+            $.each(this, function(key, value){
+                $("#faculty").append(
+                    "<img src="+"'"+ value.image +"'><br>"+
+                    "<h3>"+ value.full_name +"</h3>"+
+                    "<h3>"+ value.department +"</h3>"+
+                    "<p>" + value.bio +"</p><br>"
+                );
+            });
+        });
+    });
+});
