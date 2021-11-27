5ae56eef654e8f76b587c2a502ba59ab7d7e43a1
bshabani-UNCC <bshabani@uncc.edu>
2021-11-26 20:15:38 -0500

    Activity 16

--- itis3135/Exercise/Activity16/Shabani-Activity16.js
+++ itis3135/Exercise/Activity16/Shabani-Activity16.js
@@ -1,3 +1,14 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    var url =
+    "http://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+
+    $.getJSON(url, function(data) {
+        var html = "";
+        $.each(data.items, function(i, item) {
+            html += "<a href=" + item.media.m + " title=" + item.title + " rel=lightbox[vecta]";
+            html += "<img src=" + item.media.m + " width="
+            html += "</a>"
+        });
+        $("#newbuilding").html(html);
+    });
 });
